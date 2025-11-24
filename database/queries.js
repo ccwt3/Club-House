3e1d1379c2fbfca5248025fbd690424957a53e70
MoyyyL <moyyyl0526@gmail.com>
2025-11-24 17:46:57 -0600

    CRUD functions added to the Database (still not tested)

--- database/queries.js
+++ database/queries.js
@@ -1,9 +1,118 @@
 const pool = require("./pool");
 
-async function getAllUsers() {
+async function addUser(name, last_name, username, password) {
+  const result = await pool.query(
+    `
+    INSERT INTO users (name, last_name, username, password, role_id)
+    VALUES
+      ($1, $2, $3, $4, $5)
+    RETURNING username;
+  `,
+    [name, last_name, username, password, 3]
+  );
+
+  return result;
+}
+
+async function getUser(username) {
+  const { rows } = await pool.query(
+    `
+    SELECT * FROM users
+    WHERE username = $1;
+  `,
+    [username]
+  );
+
+  return rows;
+}
+
+async function deleteUser(username) {
+  const { rowCount } = await pool.query(
+    `
+    DELETE FROM users
+    WHERE username = $1;  
+  `,
+    [username]
+  );
+
+  return rowCount;
+}
+
+// expects an object with the modified columns and new data
+async function editUser(userData, username) {
+  const forbidden = ["role_id", "id"];
+
+  const columns = Object.keys(userData);
+  const newValues = Object.values(userData);
+
+  const conflict = columns.some((col) => forbidden.includes(col));
+  if (conflict) throw new Error("You can't do this fella");
+
+  try {
+    const setClause = columns
+      .map((col, index) => `${col} = $${index + 2}`)
+      .join(", ");
+
+    const query = `
+      UPDATE users
+      SET ${setClause}
+      WHERE username = $1
+    `;
+
+    const params = [username, ...newValues];
+
+    const { rowCount } = await pool.query(query, params);
+    return rowCount;
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
+async function addPost(title, message, author_id) {
+  const result = await pool.query(
+    `
+    INSERT INTO posts (title, message, upload_date, author_id)
+    VALUES
+      ($1, $2, $3, $4)
+    RETURNING *;
+  `,
+    [title, message, new Date(), author_id]
+  );
+
+  return result;
+}
+
+async function getAllPosts() {
   const { rows } = await pool.query(`
-    SELECT * FROM roles;
+    SELECT * FROM posts
+    ORDER BY upload_date ASC;  
   `);
 
-  return console.log(rows);
+  return rows;
+}
+
+async function deletePost(id) {
+  const { rowCount } = pool.query(
+    `
+    DELETE FROM posts
+    WHERE id = $1  
+  `,
+    [id]
+  );
+
+  return rowCount;
+}
+
+async function editPost(message, title, id) {
+  const { rowCount } = pool.query(
+    `
+    UPDATE posts
+    SET title = $1, message = $2
+    WHERE id = $3;  
+  `,
+    [title, message, id]
+  );
+
+  return rowCount;
 }
