8920a25758952ee6d6dbcd61596c6db73243b953
MoyyyL <moyyyl0526@gmail.com>
2025-11-24 12:43:53 -0600

    i forgot to add a little change

--- database/scripts/drop.js
+++ database/scripts/drop.js
@@ -13,8 +13,7 @@ async function main() {
     `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`;
 
   const client = new Client({
-    connectionString,
-    ssl: { rejectUnauthorized: false },
+    connectionString
   });
 
   try {
